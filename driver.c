078f45b7952f25c987a8fb1edcceeb4f037b462e
anshu <anshu.iiita.10@gmail.com>
2018-11-04 01:02:50 -0600

    Integrating with input parser

--- driver.c
+++ driver.c
@@ -2,9 +2,12 @@
 // Created by Arpit Jain on 11/1/18.
 //
 #include <stdio.h>
+#include "input_parser/reader.h"
 #include "input_parser/option_parser.h"
 
 int main(int argc, char *argv[]) {
 
     struct_input unprocessedInput = parse_and_get_unprocessed_input(argc, argv);
+    reader(unprocessedInput);
+
 }
