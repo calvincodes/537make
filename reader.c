7713ec296285532f93c0efd4e4087c3ab7926266
anshu <anshu.iiita.10@gmail.com>
2018-11-03 19:43:18 -0500

    Working multiple dependencies

--- reader.c
+++ reader.c
@@ -5,7 +5,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-<<<<<<< HEAD
 #include <ctype.h>
 #include "GraphNode.h"
 
@@ -22,35 +21,35 @@
 #endif //INC_537MAKE_CONSTANTS_H
 
 
-void validateTarget(char *line, unsigned int size, int lineNo){
-    if(size == 0){
-        fprintf(stderr, "%d Invalid line : %s",lineNo, line);
+void validateTarget(char *line, unsigned int size, int lineNo) {
+    if (size == 0) {
+        fprintf(stderr, "%d Invalid line : %s", lineNo, line);
         exit(EXIT_FAILURE);
     }
-    if(line[0] == ' '){
-        fprintf(stderr, "%d Invalid line : %s",lineNo, line);
+    if (line[0] == ' ') {
+        fprintf(stderr, "%d Invalid line : %s", lineNo, line);
         exit(EXIT_FAILURE);
     }
     int countColon = 0;
-    for(int i=0;i<size;i++){
+    for (int i = 0; i < size; i++) {
 //        if(countColon == 0  && line[i] == ' '){
 //            fprintf(stderr, "%d Invalid line : %s",lineNo, line);
 //            exit(EXIT_FAILURE);
 //        }
-        if(line[i] == ':')
+        if (line[i] == ':')
             countColon++;
     }
-    if(countColon != 1){
-        fprintf(stderr, "%d Invalid line : %s",lineNo, line);
+    if (countColon != 1) {
+        fprintf(stderr, "%d Invalid line : %s", lineNo, line);
         exit(EXIT_FAILURE);
     }
 }
 
-char* stripWhiteSpace(char *str) {
+char *stripWhiteSpace(char *str) {
     char *newStr = malloc(MAX_SIZE * sizeof(char));
-    int i=0;
-    while(*str != '\0'){
-        if(*str!= ' '){
+    int i = 0;
+    while (*str != '\0') {
+        if (*str != ' ') {
             *(newStr + i++) = *str;
         }
         str++;
@@ -59,22 +58,18 @@ char* stripWhiteSpace(char *str) {
     return newStr;
 }
 
-void validateCommands(char *line, unsigned int size, int lineNo){
-    if(size == 0){
-        fprintf(stderr, "%d Invalid line : %s",lineNo, line);
+void validateCommands(char *line, unsigned int size, int lineNo) {
+    if (size == 0) {
+        fprintf(stderr, "%d Invalid line : %s", lineNo, line);
         exit(EXIT_FAILURE);
     }
     int countTab = 0;
-    for(int i=0;i<size;i++){
-        if(line[i] == '\t')
+    for (int i = 0; i < size; i++) {
+        if (line[i] == '\t')
             countTab++;
     }
 }
-=======
-#include <unistd.h>
-#include "reader.h"
 
->>>>>>> 1993e6ae6dccc6f11a88b438ca44a6613963121b
 void reader() {
     FILE *file_pointer;
     char *line = (char *) malloc(MAX_SIZE * sizeof(char));
@@ -202,20 +197,21 @@ void reader() {
     int isCycleFound = is_cycle_found(curNode, graphNodeArray);
 
 //    printf("Dependency found %d", isCycleFound);
-    for(int i = 0 ;i<curNode;i++){
-        printf("%s\nDependencies ",graphNodeArray[i]->element);
-        for(int j = 0; graphNodeArray[i]->children[j] != NULL;j++){
-            printf("%s",graphNodeArray[i]->children[j]->element);
+    for (int i = 0; i < curNode; i++) {
+        printf("%s\nDependencies ", graphNodeArray[i]->element);
+        for (int j = 0; graphNodeArray[i]->children[j] != NULL; j++) {
+            printf("%s", graphNodeArray[i]->children[j]->element);
         }
         printf("\n");
 
+    }
     if (isCycleFound) {
         printf("Dependency found. Terminating.");
         exit(EXIT_FAILURE);
     }
 
 
-    GraphNode* root = graphNodeArray[0];
+    GraphNode *root = graphNodeArray[0];
     int i = 0;
 
     while (root != NULL) {
